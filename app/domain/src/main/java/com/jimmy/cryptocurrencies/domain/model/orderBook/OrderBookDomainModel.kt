f23725808980c3eb6df670300bd8f2a98e2ecccf
jgijon <jimmy.gijon@elektra.com.mx>
2022-09-21 23:26:02 -0500

    domain: add orderBook model

---
+++ app/domain/src/main/java/com/jimmy/cryptocurrencies/domain/model/orderBook/OrderBookDomainModel.kt
@@ -0,0 +1,9 @@
+package com.jimmy.cryptocurrencies.domain.model.orderBook
+
+data class OrderBookDomainModel (
+    val urlIcon: String,
+    val updatedAt: String,
+    val sequence: String,
+    val asks: List<AsksBidsValueDomainModel>,
+    val bids: List<AsksBidsValueDomainModel>,
+)
