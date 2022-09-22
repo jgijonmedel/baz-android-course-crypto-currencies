88003c2ae549f82a0f3fe39804fa8fc7c125ccc6
jgijon <jimmy.gijon@elektra.com.mx>
2022-09-22 15:44:30 -0500

    data: add availableBook data model

---
+++ app/data/src/main/java/com/jimmy/cryptocurrencies/data/repository/model/AvailableBookDataModel.kt
@@ -0,0 +1,7 @@
+package com.jimmy.cryptocurrencies.data.repository.model
+
+data class AvailableBookDataModel (
+    val book: String,
+    val maximumPrice: String,
+    val maximumValue: String,
+)
