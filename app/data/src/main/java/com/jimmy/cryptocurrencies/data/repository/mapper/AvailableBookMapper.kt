fcadab6890d3e64bbe0c5e44d7e032d3b0abf99a
jgijon <jimmy.gijon@elektra.com.mx>
2022-09-22 16:05:51 -0500

    data: add AvailableBook mapper

---
+++ app/data/src/main/java/com/jimmy/cryptocurrencies/data/repository/mapper/AvailableBookMapper.kt
@@ -0,0 +1,48 @@
+package com.jimmy.cryptocurrencies.data.repository.mapper
+
+import com.jimmy.cryptocurrencies.data.local.entity.AvailableBookEntity
+import com.jimmy.cryptocurrencies.data.network.model.response.availableBooks.AvailableBookNetworkModelResponse
+import com.jimmy.cryptocurrencies.data.repository.model.AvailableBookDataModel
+
+internal fun AvailableBookEntity.toDataModel(): AvailableBookDataModel {
+    return AvailableBookDataModel(
+        book = book,
+        maximumPrice = maximumPrice,
+        maximumValue = maximumValue,
+    )
+}
+
+@JvmName("AvailableBookEntityToDataModels")
+internal fun Collection<AvailableBookEntity>.toDataModels(): List<AvailableBookDataModel> {
+    return map { it.toDataModel() }
+}
+
+internal fun AvailableBookNetworkModelResponse.toDataModel(): AvailableBookDataModel {
+    return AvailableBookDataModel(
+        book = book,
+        maximumPrice = maximumPrice,
+        maximumValue = maximumValue,
+    )
+}
+
+@JvmName("AvailableBookNetworkModelResponseToDataModels")
+internal fun Collection<AvailableBookNetworkModelResponse>.toDataModels(): List<AvailableBookDataModel> {
+    return map { it.toDataModel() }
+}
+
+internal fun AvailableBookNetworkModelResponse.toEntity(): AvailableBookEntity {
+    return AvailableBookEntity(
+        book = book,
+        minimumPrice = minimumPrice,
+        maximumPrice = maximumPrice,
+        minimumAmount = minimumAmount,
+        maximumAmount = maximumAmount,
+        minimumValue = minimumValue,
+        maximumValue = maximumValue,
+    )
+}
+
+@JvmName("AvailableBookNetworkModelResponseToEntities")
+internal fun Collection<AvailableBookNetworkModelResponse>.toEntities(): List<AvailableBookEntity> {
+    return map { it.toEntity() }
+}
