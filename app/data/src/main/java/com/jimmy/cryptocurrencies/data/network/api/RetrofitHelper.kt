01a3127513447cdff0ace013bc4e22281e48b652
jgijon <jimmy.gijon@elektra.com.mx>
2022-09-22 17:12:51 -0500

    data: add logging interceptor

--- app/data/src/main/java/com/jimmy/cryptocurrencies/data/network/api/RetrofitHelper.kt
+++ app/data/src/main/java/com/jimmy/cryptocurrencies/data/network/api/RetrofitHelper.kt
@@ -1,14 +1,25 @@
 package com.jimmy.cryptocurrencies.data.network.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 internal object RetrofitHelper {
     private const val baseUrl = CryptoCurrenciesApiPaths.BASE_URL
 
+    private val loggingInterceptor = HttpLoggingInterceptor().also {
+        it.level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+    
     private fun getRetrofit(): Retrofit {
         return  Retrofit.Builder()
             .baseUrl(baseUrl)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
